09cf328e4d1290d925cc6e827b38a4b06fe2fab0
awezmehtab <awezmehtab@users.noreply.github.com>
2025-02-18 18:44:20 +0530

    Images added in report

--- A2/automator.py
+++ A2/automator.py
@@ -1,13 +1,16 @@
 import subprocess
 from joblib import Parallel, delayed
+import matplotlib.pyplot as plt
 
 
-def run_command(command: str = '', shell_output: bool = False) -> str:
+def run_command(command: str = "", shell_output: bool = False) -> str:
     output = ""
-    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+    proc = subprocess.Popen(
+        command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True
+    )
     for line in proc.stdout:
         if shell_output:
-            print(line, end='')
+            print(line, end="")
         output += line
     proc.wait()
     output = output.strip()
@@ -25,51 +28,87 @@ def run_command(command: str = '', shell_output: bool = False) -> str:
 
 
 def cadical_run(
-        filehash: str,
-        restart: bool = True,
-        restartint: int = 2,
-        restartmargin: int = 10,
-        restartreusetrail: bool = True,
-        reluctant: int = 1024,
-        reluctantmax: int = 1048576,
-        shell_output: bool = False
-    ):
-
+    filehash: str,
+    restart: bool = True,
+    restartint: int = 2,
+    restartmargin: int = 10,
+    restartreusetrail: bool = True,
+    reluctant: int = 1024,
+    reluctantmax: int = 1048576,
+    shell_output: bool = False,
+):
     file = f"{filehash}*.cnf.xz"
-    benchmarks_directory = './benchmarks'
+    benchmarks_directory = "./benchmarks"
 
-    options = f'--restart={str(restart).lower()} \
+    options = f"--restart={str(restart).lower()} \
     --restartint={restartint} \
     --restartmargin={restartmargin} \
     --restartreusetrail={str(restartreusetrail).lower()} \
     --reluctant={reluctant} \
-    --reluctantmax={reluctantmax}'
+    --reluctantmax={reluctantmax}"
 
     # Expecting cadical is in the path and you are running this in A2 directory
-    command = f'cadical {benchmarks_directory}/{file} {options}'
-    command_output = run_command(command=command, shell_output=shell_output).split('\n')[-7:-4]
+    command = f"cadical {benchmarks_directory}/{file} {options}"
+    command_output = run_command(command=command, shell_output=shell_output).split(
+        "\n"
+    )[-7:-4]
     return tuple(float(command_output[i].split()[-2]) for i in [0, 2])
-        
 
-def forloop(i: int) -> None:
-    out = cadical_run(filehash='000a41cdca43be89ed62ea3abf2d0b64', restartmargin=i)[0]
-    print(out)
-    print('----------')
-    return i, out
 
-filehash_out1 = Parallel(n_jobs=-1)(delayed(forloop)(i) for i in range(101))
+def get_data(
+    filehash: str, param: str, param_range: range, memory: bool = False
+) -> list:
+    def forloop(i: int) -> tuple:
+        out = cadical_run(filehash=filehash, **({param: i}))[int(memory)]
+        print(f"{i = }, {out = }")
+        print("----------")
+        return i, out
 
-print('\a')
+    filehash_out: list = list(
+        Parallel(n_jobs=-1)(delayed(forloop)(i) for i in param_range)
+    )
 
-import matplotlib.pyplot as plt
+    return filehash_out
 
-# Extract x and y coordinates from filehash_out1
-x_vals = [point[0] for point in filehash_out1]
-y_vals = [point[1] for point in filehash_out1]
 
-plt.plot(x_vals, y_vals, marker='o')
-plt.xlabel("X axis")
-plt.ylabel("Y axis")
-plt.title("Filehash Output Plot")
-plt.grid(True)
-plt.show()
+def plot_data(
+    filehash: str, param: str, param_range: range, memory: bool = False
+) -> None:
+    filehash_out1: list = get_data("31c4aac", param, param_range, memory=memory)
+
+    # Extract x and y coordinates from filehash_out1
+    x_vals = [point[0] for point in filehash_out1]
+    y_vals = [point[1] for point in filehash_out1]
+
+    plt.step(x_vals, y_vals, where="post", marker="o")
+    plt.xlabel(param)
+    plt.ylabel("CPU Time Taken")
+    plt.title(f"{param} vs {'memory' if memory else 'time'}")
+    plt.grid(True)
+    plt.savefig(f"images/{param}-{int(memory)}.png")
+    plt.show()
+
+
+def main() -> None:
+    parameters = {
+        "restartint",
+        "restartmargin",
+        "reluctant",
+        "reluctantmax",
+    }
+
+    for param in parameters:
+        param_range = range(1)
+        if param == "restartmargin":
+            param_range = range(0, 100, 1)
+        else:
+            param_range = range(0, int(1e4), 100)
+
+        print(f"{param}: MEMORY")
+        plot_data("31c4aac", param, param_range, memory=True)
+        print(f"{param}: TIME")
+        plot_data("31c4aac", param, param_range, memory=False)
+
+
+if __name__ == "__main__":
+    main()
